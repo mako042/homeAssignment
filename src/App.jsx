61be6aab6a7f2699470d08b229f64304872ef3c6
DevilOps <leoshanw@gmail.com>
2026-04-29 22:02:58 +0500

    Один общий state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
+import { useState } from 'react'
 import Footer from './components/footer'
 import Header from './components/header'
 import Content from './components/content'
+import './App.css'
 
 function App() {
+  const [pageType, setPageType] = useState('tv')
+  const [cart, setCart] = useState({})
+
   return (
     <>
-      <Header />
+      <Header 
+        pageType={pageType} 
+        setPageType={setPageType} 
+        cart={cart} 
+      />
       <main className='flex-1 px-5'>
-        <Content />
+        <Content 
+          pageType={pageType} 
+          setPageType={setPageType} 
+          cart={cart} 
+          setCart={setCart} 
+        />
       </main>
       <Footer />
     </>
-  );
+  )
 }
 
 export default App
